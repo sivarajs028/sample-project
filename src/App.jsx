74a4dc0a9d4667143f2ae7aece4ed764f4c41fd7
sivarajs028 <82sivaraj@gmail.com>
2024-10-04 10:15:18 +0000

    ac

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-export default function App() {
-  return(
-    <div>
-      Sivaraj
-    </div>
-  );
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        Sivaraj
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
+
+export default App
